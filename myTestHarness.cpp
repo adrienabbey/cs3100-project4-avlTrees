a04c7945c2d9bdf368bb2673f6984703517f0449
Adrien Abbey <abbey.12@wright.edu>
2023-03-10 15:55:47 -0500

    basic cpp file outline

---
+++ myTestHarness.cpp
@@ -0,0 +1,12 @@
+// Adrien Abbey, CS-3100-02, Mar. 10, 2023
+// Project #4: Indexing with AVL Trees
+// Visual Studio Code Server, Ubuntu 22.04, g++/gdb
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
