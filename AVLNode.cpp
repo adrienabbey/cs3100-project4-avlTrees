df6ba6898204e43c1222950cf8940016febe1dd7
Adrien Abbey <abbey.12@wright.edu>
2023-03-10 16:16:23 -0500

    basic initial cpp file outlines

---
+++ AVLNode.cpp
@@ -0,0 +1,48 @@
+// Adrien Abbey, CS-3100-02, Mar. 10, 2023
+// Project #4: Indexing with AVL Trees
+// Visual Studio Code Server, Ubuntu 22.04, g++/gdb
+
+#include <string>
+#include "AVLNode.h"
+
+using namespace std;
+
+/* Constructors */
+AVLNode::AVLNode(int newKey, string newValue)
+{
+    key = newKey;
+    value = newValue;
+}
+
+/* Methods */
+int AVLNode::getKey()
+{
+}
+
+int AVLNode::setKey(int value)
+{
+}
+
+string AVLNode::getValue()
+{
+}
+
+string AVLNode::setValue(int value)
+{
+}
+
+AVLNode AVLNode::getLeftChild()
+{
+}
+
+AVLNode AVLNode::setLeftChild(AVLNode node)
+{
+}
+
+AVLNode AVLNode::getRightChild()
+{
+}
+
+AVLNode AVLNode::setRightChild(AVLNode node)
+{
+}
---
+++ AVLTree.cpp
@@ -0,0 +1,41 @@
+// Adrien Abbey, CS-3100-02, Mar. 10, 2023
+// Project #4: Indexing with AVL Trees
+// Visual Studio Code Server, Ubuntu 22.04, g++/gdb
+
+#include <iostream>
+#include "AVLTree.h"
+
+using namespace std;
+
+/* Constructors */
+
+AVLTree::AVLTree()
+{
+    // Empty AVLTree constructor.
+    AVLNode *root = nullptr;
+}
+
+/* Methods */
+bool AVLTree::insert(int key, string value)
+{
+}
+
+bool AVLTree::find(int key, string &value)
+{
+}
+
+vector<string> AVLTree::findRange(int lowkey, int highkey)
+{
+}
+
+int AVLTree::getHeight()
+{
+}
+
+int AVLTree::getSize()
+{
+}
+
+friend ostream &operator<<(ostream &os, const AVLTree &me)
+{
+}
