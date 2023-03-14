484cac76f3206e23b2d58ce84fc4014012928235
Adrien Abbey <abbey.12@wright.edu>
2023-03-14 11:11:48 -0400

    started work on insert method, incomplete

--- AVLTree.cpp
+++ AVLTree.cpp
@@ -17,14 +17,38 @@ AVLTree::AVLTree()
 }
 
 /* Methods */
-bool AVLTree::insert(int key, string value)
+bool AVLTree::insert(int newKey, string newValue)
 {
     // Inserts the given key and value into this tree.
 
     // REMINDER: Increase tree size when insert is successful!
 
-    // FIXME: test code, inserts just a root node:
-    root = new AVLNode(key, value);
+    // If there is no root, the new node becomes root:
+    if (root == nullptr)
+    {
+        root = new AVLNode(newKey, newValue);
+        // A node was successfully inserted.  Do not continue.
+        return true;
+    }
+
+    // Otherwise, start walking down the tree.
+    // Create a pointer to track where we are in the tree:
+    AVLNode *currentNode = root;
+
+    // While there is a node to track:
+    while (currentNode != nullptr)
+    {
+        // If the new key is less than the current key:
+        if (newKey < currentNode->getKey())
+        {
+            // And if the current key has no left child:
+            if (currentNode->getLeftChild() == nullptr)
+            {
+                // Then set the current node's left child to the new node:
+                currentNode->setLeftChild(new AVLNode(newKey, newValue));
+            }
+        }
+    }
 
     // FIXME: return 'false' if the key already exists:
     return true;
