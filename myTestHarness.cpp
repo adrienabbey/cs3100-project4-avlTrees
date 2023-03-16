ed84dc63079c6158ebd88e4056ef3790c908e377
Adrien Abbey <abbey.12@wright.edu>
2023-03-16 15:27:57 -0400

    added more test nodes to test rebalancing tree

--- myTestHarness.cpp
+++ myTestHarness.cpp
@@ -15,6 +15,12 @@ int main()
     cout << newTree.insert(50, "Fifty") << endl;
     cout << newTree.insert(30, "Thirty") << endl;
     cout << newTree.insert(40, "Forty") << endl; // should fail
+    cout << newTree.insert(20, "Twenty") << endl;
+    cout << newTree.insert(10, "Ten") << endl;
+    cout << newTree.insert(5, "Five") << endl;
+    cout << newTree.insert(60, "Sixty") << endl;
+    cout << newTree.insert(70, "Seventy") << endl;
+    cout << newTree.insert(80, "Eighty") << endl;
 
     return 0;
 }
